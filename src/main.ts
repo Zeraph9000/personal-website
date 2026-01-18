a6dbf97e539bf85f0779b260ed262b59525455b5
Andrew <z5621138@ad.unsw.edu.au>
2026-01-18 21:14:37 +0700

    fixing

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { initNavigation } from './navigation';
 import { initProjects } from './projects';
+import { initSkills } from './skills';
 import './style.css';
 
 // Initialize all modules
 initNavigation();
 initProjects();
+initSkills();
